05b84071a762558b5c866ee703b2ed93a290f7ee
Krati Agarwal <kratiagarwal7812@gmail.com>
2026-02-24 10:26:32 +0530

    js

--- script.js
+++ script.js
@@ -1,36 +1,67 @@
 console.log("Hello World!");
-document.write("Hello World!");
+// document.write("Hello World!");
 
-var num=10;
-console.log(num);
-console.log(typeof(num));
+// var num=10;
+// console.log(num);
+// console.log(typeof(num));
 
-num=false;
-console.log(num);
-console.log(typeof(num));
+// num=false;
+// console.log(num);
+// console.log(typeof(num));
 
-num=null;
-console.log(num);
-console.log(typeof(num));
+// num=null;
+// console.log(num);
+// console.log(typeof(num));
 
-num=undefined;
-console.log(num);
-console.log(typeof(num));
+// num=undefined;
+// console.log(num);
+// console.log(typeof(num));
 
-num="hello";
-console.log(num);
-console.log(typeof(num));
+// num="hello";
+// console.log(num);
+// console.log(typeof(num));
 
 
-const sym1= Symbol(4);
-console.log(sym1);
+// const sym1= Symbol(4);
+// console.log(sym1);
 
-const sym2= Symbol(4);
-console.log(sym2);
+// const sym2= Symbol(4);
+// console.log(sym2);
 
-if(sym1==sym2){
-    console.log("true");
+// if(sym1==sym2){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+// let age=18;
+// var ans= (age==18) ? ("can vote") : ("cannot vote");
+
+// console.log(ans);
+
+// let a=6;
+// let b=2;
+// let result;
+
+// result= a+b;
+// console.log(result);
+// result= a-b;
+// console.log(result);
+// result=a*b;
+// console.log(result);
+// result=a/b;
+// console.log(result);
+// result=a%b;
+// console.log(result);
+
+var count;
+
+document.write("Starting loop"+"<br/>");
+
+for(count=0;count<10;count++){
+    document.write("current count:"+count);
+    document.write("<br/>");
 }
-else{
-    console.log("false");
-}
+
+document.write("loop stopped!");
